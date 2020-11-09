7d41910f3451eea84ecc9bb948f10e967214ac91
左倾月 <3027929699@qq.com>
2020-11-10 00:17:55 +0800

    结构调整

--- Unity/Assets/ModelView/Mono/Test/PathfindingTestMono.cs
+++ Unity/Assets/ModelView/Mono/Test/PathfindingTestMono.cs
@@ -1,12 +1,14 @@
 ﻿
 using DG.Tweening;
-using GridPathfindingSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace ET
 {
+    /// <summary>
+    /// 测试
+    /// </summary>
     public class PathfindingTestMono : MonoBehaviour
     {
         [SerializeField]
@@ -17,7 +19,7 @@ namespace ET
         private Transform walkAble;
         [SerializeField]
         private Transform unwalkAble;
-        private GridPathfinding gridPathfinding;
+        //private GridPathfinding gridPathfinding;
 
         [SerializeField]
         private Transform start, end, pathPrefab;
@@ -31,47 +33,42 @@ namespace ET
 
         private void Start()
         {
-            gridPathfinding = new GridPathfindingSystem.GridPathfinding(new Vector3(left.position.x, left.position.z, 0), new Vector3(right.position.x, right.position.z, 0), 1.5f);
-            gridPathfinding.Raycast3DWalkable();
+            //gridPathfinding = new GridPathfindingSystem.GridPathfinding(new Vector3(left.position.x, left.position.z, 0), new Vector3(right.position.x, right.position.z, 0), 1.5f);
+            //gridPathfinding.Raycast3DWalkable();
         }
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.A))
             {
-                tran = GameObject.Instantiate(pathPrefab, start.position, Quaternion.identity, GameObject.Find("Game").transform).transform;
-                var pathRoute = gridPathfinding.GetPathRoute(new Vector3(start.position.x, start.position.z), new Vector3(end.position.x, end.position.z));
-                path = new WayPath();
-                var list = pathRoute.pathVectorList;
-                path.Init(list, list[0], 0.2f, 1f);
-                pathIndex = 0;
-                followParthing = true;
-                var first = path.LookPoints[0];
-                tran.LookAt(new Vector3(first.x, 0, first.y));
+                //tran = GameObject.Instantiate(pathPrefab, start.position, Quaternion.identity, GameObject.Find("Game").transform).transform;
+                //var pathRoute = gridPathfinding.GetPathRoute(new Vector3(start.position.x, start.position.z), new Vector3(end.position.x, end.position.z));
+                //path = new WayPath();
+                //var list = pathRoute.pathVectorList;
+                //path.Init(list, list[0], 0.2f, 1f);
+                //pathIndex = 0;
+                //followParthing = true;
+                //var first = path.LookPoints[0];
+                //tran.LookAt(new Vector3(first.x, 0, first.y));
 
             }
-            ///y->-z
-            ///x->x
-            ///z->y
-            ///(x,0,z)->(x,-z=0,y)
-            ///pos =（x,z,-y）
-            if (followParthing)
-            {
+            //if (followParthing)
+            //{
 
-                while (path.TurnBoundaries[pathIndex].HasCrossLine(new Vector2(tran.position.x, tran.position.z)))
-                {
-                    if (pathIndex >= path.FinishLineIndex)
-                    {
-                        followParthing = false;
-                        return;
-                    }
-                    else
-                        pathIndex++;
-                }
-                Vector3 point = path.LookPoints[pathIndex];
-                Quaternion quaternion = Quaternion.LookRotation(new Vector3(point.x, 0, point.y) - new Vector3(tran.position.x, 0, tran.position.z));
-                tran.rotation = Quaternion.Lerp(tran.rotation, quaternion, turnSpeed * Time.deltaTime);
-                tran.Translate(new Vector3(0, 0, 1) * Time.deltaTime * moveSpeed, Space.Self);
-            }
+            //    while (path.TurnBoundaries[pathIndex].HasCrossLine(new Vector2(tran.position.x, tran.position.z)))
+            //    {
+            //        if (pathIndex >= path.FinishLineIndex)
+            //        {
+            //            followParthing = false;
+            //            return;
+            //        }
+            //        else
+            //            pathIndex++;
+            //    }
+            //    Vector3 point = path.LookPoints[pathIndex];
+            //    Quaternion quaternion = Quaternion.LookRotation(new Vector3(point.x, 0, point.y) - new Vector3(tran.position.x, 0, tran.position.z));
+            //    tran.rotation = Quaternion.Lerp(tran.rotation, quaternion, turnSpeed * Time.deltaTime);
+            //    tran.Translate(new Vector3(0, 0, 1) * Time.deltaTime * moveSpeed, Space.Self);
+            //}
 
         }
 
